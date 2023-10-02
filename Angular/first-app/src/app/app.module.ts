acecae3108c93bb92ca740d0acba836185871e0f
Oscar Olsson <happybits@gmail.com>
2023-10-02 11:14:42 +0200

    -

--- Angular/first-app/src/app/app.module.ts
+++ Angular/first-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderTextComponent } from './header-text/header-text.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderTextComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular/first-app/src/app/header-text/header-text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-text',
+  templateUrl: './header-text.component.html',
+  styleUrls: ['./header-text.component.css']
+})
+export class HeaderTextComponent {
+
+}
