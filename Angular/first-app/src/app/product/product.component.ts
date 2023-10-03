933d55373528e743ac2d66563b0b705769482492
Oscar Olsson <happybits@gmail.com>
2023-10-03 11:31:51 +0200

    -

--- Angular/first-app/src/app/product/product.component.ts
+++ Angular/first-app/src/app/product/product.component.ts
@@ -10,16 +10,24 @@ export class ProductComponent {
   @Input() image = 'missing-image.jpg'
   @Input() name = '<No name>'
   @Input() description = '<No description>'
-  @Input() isInStock = false 
+  @Input() isInStock = false
+
+  amount = 0
 
   isCompact = false
 
-  constructor(private displayMode: DisplayModeService){
-    
+  constructor(private displayMode: DisplayModeService) {
+
     this.displayMode.currentCompactStatus.subscribe(
 
       status => this.isCompact = status
 
     )
   }
+
+  adjustAmount(diff: number) {
+    this.amount += diff
+    this.amount = Math.max(0, this.amount)
+  }
+
 }
